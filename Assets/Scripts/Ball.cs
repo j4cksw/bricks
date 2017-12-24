9ec5aad881822ac129137467cd861ba8f78e5543
Prayoch Rujira <prayoch@clockupstudio.com>
2017-12-24 21:30:36 +0700

    Add ball physics -Ball can destroy bricks

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	private bool gameStarted = false;
+	private Vector3 paddleVector;
+	// Use this for initialization
+	void Start () {
+		paddleVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameStarted){
+			return;
+		}
+
+		this.transform.position = paddle.transform.position + paddleVector;
+
+		if(Input.GetMouseButtonDown(0)) {
+			print("Mouse clicked");
+			gameStarted = true;
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 5f);
+		}
+	}
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits;
+	public int timesHit;
+
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timesHit = 0;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		print("Ouch!!");
+		timesHit++;
+
+		if(timesHit == maxHits) {
+			print("Destroyed!");
+			Destroy(gameObject);
+		}
+	}
+}
