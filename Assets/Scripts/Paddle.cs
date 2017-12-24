ea21f90d45d8afac1a08cb91ee92bea14273701b
Prayoch Rujira <prayoch@clockupstudio.com>
2017-12-24 20:57:01 +0700

    First commit - Add all screen layout items - Paddle move by following the mouse cursor

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 targetPose = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0f));
+
+		this.transform.position = new Vector3(Mathf.Clamp(targetPose.x, -3.5f, 3.5f), this.transform.position.y, this.transform.position.z);
+	}
+}
