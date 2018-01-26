82daabf920f91a0a4c27486018c9f0564d03ebb3
Prayoch Rujira <prayoch@clockupstudio.com>
2018-01-26 11:25:04 +0700

    Add integration test experiment

---
+++ Assets/Scripts/GameStateTests.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStateTests {
+
+	// [Test]
+	// public void GameStateTestsSimplePasses() {
+		
+	// }
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator GameStateTestsWithEnumeratorPasses() {
+		SceneManager.LoadScene("main");
+		Assert.That(SceneManager.sceneCount, Is.EqualTo(2));
+		yield return null;
+	}
+}
