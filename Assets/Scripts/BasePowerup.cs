b6a90fd07d50a0e22be1a314236f86561d2ae29e
Prayoch Rujira <prayoch@clockupstudio.com>
2018-01-27 13:01:38 +0700

    Add BasePowerup script

---
+++ Assets/Scripts/BasePowerup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D), typeof(AudioSource))]
+public class BasePowerup : MonoBehaviour {
+
+	public float dropSpeed = 1;
+	public AudioClip sound;
+
+	void Start () {
+		GetComponent<AudioSource>().playOnAwake = false;
+	}
+	
+	IEnumerator OnTriggerEnter2D(Collider2D other) {
+		if( other.name == "Paddle") {
+			OnPickup();
+
+			GetComponent<Collider2D>().enabled = false;
+			GetComponent<Renderer>().enabled = false;
+
+			GetComponent<AudioSource>().pitch = Time.timeScale;
+
+			GetComponent<AudioSource>().PlayOneShot(sound);
+			yield return new WaitForSeconds(sound.length);
+		}
+	}
+
+	protected virtual void OnPickup() {
+
+	}
+}
