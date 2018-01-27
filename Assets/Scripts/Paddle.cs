63801207ae672a81e52f1625411b0480db4b51dd
Prayoch Rujira <prayoch@clockupstudio.com>
2018-01-27 10:36:42 +0700

    Play paddle sound

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public AudioClip sound;
+
 	void Update () {
 
 		Vector3 mousePos = Input.mousePosition;
 		Vector3 targetPose = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0f));
 
 		this.transform.position = new Vector3(Mathf.Clamp(targetPose.x, -3.5f, 3.5f), this.transform.position.y, this.transform.position.z);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		GetComponent<AudioSource>().pitch = Time.timeScale;
+
+		GetComponent<AudioSource>().PlayOneShot(sound);
 	}
 }
