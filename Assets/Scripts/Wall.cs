05c14109fdae951b2d445d6241ce6ae7b641ee21
Prayoch Rujira <prayoch@clockupstudio.com>
2018-01-27 10:30:36 +0700

    Play wall-hit sound when the ball hit the wall

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Wall : MonoBehaviour {
+
+	public AudioClip sound;
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		GetComponent<AudioSource>().PlayOneShot(sound);
+	}
+}
